ea939f0214cb8fbdb847a7bfaa962b12c32d362f
otienoelvis <sevileelvis@gmail.com>
2023-12-04 02:37:48 +0300

    login screen api, select csv, screen guards

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {LoginType} from "../types/LoginType";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private tokenKey = 'authToken';
+  private userDataKey = 'userData';
+
+  constructor(private http: HttpClient) { }
+
+  loginUser(loginPayload : LoginType){
+    let url = "http://localhost:3001/api/v1/login";
+
+
+    return this.http.post(url, loginPayload)
+  }
+
+  saveUser(data: any): void {
+    localStorage.removeItem(this.userDataKey);
+    localStorage.setItem(this.userDataKey, JSON.stringify(data));
+
+    this.saveToken(data.token);
+  }
+
+  saveToken(token: string): void {
+    localStorage.removeItem(this.tokenKey);
+    localStorage.setItem(this.tokenKey, token);
+  }
+
+
+
+  getUserData(): any | null {
+    const dataString = localStorage.getItem(this.userDataKey);
+    return dataString ? JSON.parse(dataString) : null;
+  }
+
+  removeUserData(): void {
+    localStorage.removeItem(this.userDataKey);
+    localStorage.removeItem(this.tokenKey);
+  }
+
+  removeToken(): void {
+    localStorage.removeItem(this.tokenKey);
+  }
+}
